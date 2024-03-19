efcb61842f662c2a6e3b914ca81f9f53c7cfd9d2
Leonid29 <job@leonid29-developer.ru>
2024-03-19 22:15:15 +0400

    Добавьте файлы проекта.

---
+++ System_For_AnalyzingProject/Authorization.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace System_For_AnalyzingProject
+{
+    partial class Authorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TB_Login = new System.Windows.Forms.TextBox();
+            this.TB_Password = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Button_Enter = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(101, 45);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Авторизация";
+            // 
+            // TB_Login
+            // 
+            this.TB_Login.Location = new System.Drawing.Point(93, 146);
+            this.TB_Login.Name = "TB_Login";
+            this.TB_Login.Size = new System.Drawing.Size(194, 29);
+            this.TB_Login.TabIndex = 2;
+            this.TB_Login.Text = "Gulding28@kur";
+            // 
+            // TB_Password
+            // 
+            this.TB_Password.Location = new System.Drawing.Point(93, 222);
+            this.TB_Password.Name = "TB_Password";
+            this.TB_Password.Size = new System.Drawing.Size(194, 29);
+            this.TB_Password.TabIndex = 4;
+            this.TB_Password.Text = "Sp(9hv*3pr&O";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(93, 198);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(194, 21);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Пароль";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Button_Enter
+            // 
+            this.Button_Enter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Button_Enter.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Button_Enter.Location = new System.Drawing.Point(87, 289);
+            this.Button_Enter.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.Button_Enter.Name = "Button_Enter";
+            this.Button_Enter.Size = new System.Drawing.Size(209, 37);
+            this.Button_Enter.TabIndex = 5;
+            this.Button_Enter.Text = "Авторизироваться";
+            this.Button_Enter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Button_Enter.Click += new System.EventHandler(this.Button_Enter_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(93, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Логин";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(515, 94);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(343, 81);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(515, 252);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(343, 89);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(59, 418);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 19);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Авторизация";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(938, 94);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(173, 33);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Авторизация";
+            // 
+            // Authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1356, 745);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Button_Enter);
+            this.Controls.Add(this.TB_Password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TB_Login);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Authorization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TB_Login;
+        private System.Windows.Forms.TextBox TB_Password;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Button_Enter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ System_For_AnalyzingProject/Encryption.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace System_For_AnalyzingProject
+{
+    //public class Encryption
+    //{
+    //    /// <summary> Допустимые наборы символов </summary>
+    //    private const string
+    //        ENG = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    //        RUS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
+    //        Numbers = "0123456789",
+    //        Symbols = "!@#$%^&*()+=_-?:;~",
+    //        Hieroglyphs = "用而不前博均解斯给它数その学究合何に私い降れス雨람을없아죄수러壘테个洒澜谜铝铜磨瘾瘸凝辨모" +
+    //        "빛를슴용囤员金范围园且十丫三大币只田소낯과근니대火木上中口人先青山川森雲王外牛玉見工黒乖拧夷岁日以尘示吮我" +
+    //        "含집검건패달품칠지비질隶車豸貝門鬯馬鼻齒龜龠隹非出書色心西雪千組竹町道年風百文米北名明侧殃品哪衍香侠网众阶";
+
+    //    /// <summary> Активация или деактивация наборов символов </summary>
+    //    private bool[] Bools = { false, false, false, false };
+
+    //    /// <summary> Смещение символов </summary>
+    //    private int Shift = 1;
+
+    //    /// <summary> Ключ </summary>
+    //    private string Key = "";
+
+    //    /// <summary> Символы столбцов </summary>
+    //    private string Headlines = "";
+
+    //    /// <summary> Таблица кодирования </summary>
+    //    private char[,] EncodingTable;
+
+    //    /// <summary> Конструктор </summary>
+    //    /// <param name="SetBools">Активация или деактивация наборов символов</param>
+    //    /// <param name="SetKey">Назначение ключа</param>
+    //    /// <param name="SetShift">Назначение смещения</param>
+    //    public Encryption(bool[] SetBools, string SetKey, int SetShift)
+    //    {
+    //        try
+    //        {
+    //            Bools = SetBools; Key = SetKey; Shift = SetShift;
+
+    //            if (Bools[0]) Headlines += ENG + ENG.ToLower();
+    //            if (Bools[1]) Headlines += RUS + RUS.ToLower();
+    //            if (Bools[2]) Headlines += Numbers;
+    //            if (Bools[3]) Headlines += Symbols;
+
+    //            EncodingTable = new char[Headlines.Length, Headlines.Length];
+
+    //            for (int I1 = 0, Index = 0; I1 < EncodingTable.GetLength(0); I1++)
+    //            {
+    //                for (int I2 = 0; I2 < EncodingTable.GetLength(1); I2++, Index += Shift)
+    //                {
+    //                    if (Index > Hieroglyphs.Length - 1) Index -= Hieroglyphs.Length;
+    //                    EncodingTable[I1, I2] = Hieroglyphs[Index];
+    //                }
+    //            }
+    //        }
+    //        catch (Exception Ex)
+    //        {
+    //            MessageBox.Show($"Encryption#Encryption\n{Ex.Message}", "Неизвестная ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    //        }
+    //    }
+
+    //    /// <summary> Поиск индекса символа в массиве символов </summary>
+    //    /// <param name="Li">Искомый символ</param>
+    //    private int Indexing(char Li)
+    //    {
+    //        try
+    //        {
+    //            for (int I1 = 0; I1 < Headlines.Length; I1++)
+    //                if (Li == Headlines[I1]) return I1;
+    //            throw new Exception();
+    //        }
+    //        catch (Exception Ex)
+    //        {
+    //            MessageBox.Show($"Encryption#Indexing\n{Ex.Message}", "Неизвестная ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); return -1;
+    //        }
+    //    }
+
+    //    /// <summary> Закодирование </summary>
+    //    /// <param name="Text">Текст для закодирования</param>
+    //    public string Encoding(string Text)
+    //    {
+    //        try
+    //        {
+    //            string Final = "", StringKey = "";
+
+    //            for (int I1 = 0; StringKey.Length < Text.Length; I1++)
+    //            {
+    //                if (I1 == Key.Length) I1 = 0;
+    //                StringKey += Key[I1];
+    //            }
+
+    //            for (int I1 = 0; I1 < Text.Length; I1++)
+    //            {
+    //                int[] Index = { Indexing(Text[I1]), Indexing(StringKey[I1]) };
+    //                if (Index[0] != -1 & Index[1] != -1)
+    //                    Final += EncodingTable[Index[0], Index[1]];
+    //            }
+
+    //            return Final;
+    //        }
+    //        catch (Exception Ex)
+    //        {
+    //            MessageBox.Show($"Encryption#Encoding\n{Ex.Message}", "Неизвестная ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); return "ERROR";
+    //        }
+    //    }
+
+    //    /// <summary> Раскодирование </summary>
+    //    /// <param name="Text">Текст для раскодирование</param>
+    //    public string Decoding(string Text)
+    //    {
+    //        try
+    //        {
+    //            string Final = "", StringKey = "";
+
+    //            for (int I1 = 0; StringKey.Length < Text.Length; I1++)
+    //            {
+    //                if (I1 == Key.Length) I1 = 0;
+    //                StringKey += Key[I1];
+    //            }
+
+    //            for (int I1 = 0; I1 < Text.Length; I1++)
+    //            {
+    //                int Index = Indexing(StringKey[I1]);
+
+    //                for (int I2 = 0; I2 < Headlines.Length; I2++)
+    //                    if (EncodingTable[I2, Index] == Text[I1]) Final += Headlines[I2];
+    //            }
+
+    //            return Final;
+    //        }
+    //        catch (Exception Ex)
+    //        {
+    //            MessageBox.Show($"Encryption#Decoding\n{Ex.Message}", "Неизвестная ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); return "ERROR";
+    //        }
+    //    }
+    //}
+}
