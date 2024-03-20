bea2843673ba3598b87c007ca1d3283e5f38af4e
Leonid29 <job@leonid29-developer.ru>
2024-03-21 00:25:46 +0400

    Добавление директорий‭ «Тестируемые элементы» и ‭«Типы тестирования»

---
+++ System_For_AnalyzingProject/Directories/TestedElemnets.cs
@@ -0,0 +1,20 @@
+﻿namespace System_For_AnalyzingProject
+{
+    /// <summary> Директория. Тестируемые элементы </summary>
+    public enum TestedElemnets
+    {
+        Button,
+        PiсtureBox,
+        LinkLabel,
+        Label,
+        TextBox,
+        DataTable,
+        TableLayoutPanel,
+        ConnectingDatabaseToForm,
+        DBTable,
+        Panel,
+        Form,
+        CorrectnessDataDisplayOnForms,
+        System
+    }
+}
---
+++ System_For_AnalyzingProject/Directories/TestingTypes.cs
@@ -0,0 +1,14 @@
+﻿namespace System_For_AnalyzingProject
+{
+    /// <summary> Директория. Типы тестирования </summary>
+    public enum TestingTypes
+    {
+        ManualTesting,
+        IntegrationTesting,
+        WhiteBoxTesting,
+        DescendingTesting,
+        ModularTesting,
+        AcceptanceTesting,
+        LoadTesting
+    }
+}
